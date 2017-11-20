124db715d84f5e22920640f2b9b10b0b71f45c09
zy6zhou <zy6zhou@edu.uwaterloo.ca>
2017-11-20 14:23:59 -0500

    removed old files

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -1,36 +1,41 @@
-#include <SoftwareSerial.h>
-SoftwareSerial mySerial(12, 13); // RX, TX
+#include <SoftwareSerial.h> 
+#include <string.h>
+#include <stdlib.h>
 
-#define IN1 2 // L298N back (blue)
-#define IN2 3 // L298N forward (green)
-#define IN3 4 // L298N left (yellow)
-#define IN4 5 // L298N right (orange)
+SoftwareSerial bluetoothConnection(12, 13); // RX, TX
+
+#define IN2 3        // L298N Forward  (green)
+#define IN3 4        // L298N Left (yellow)
+#define IN4 5        // L298N Right (orange)
+#define IN1 6        // L298N Backward (blue)
+
+#define FORWARD '0'
+#define BACKWARD '1'
+#define LEFT '2'
+#define RIGHT '3'
+#define STRAIGHT '4'
+#define STOP '9'
+
 #define trigRight A0 // HC-SR04 trig right
 #define echoRight A1 // HC-SR04 echo right
 #define trigLeft A2  // HC-SR04 trig left
 #define echoLeft A3  // HC-SR04 echo left
-#define trigBack A4  // HC-SR04 trig forward
-#define echoBack A5  // HC-SR04 echo back
+#define trigCentre A4  // HC-SR04 trig forward
+#define echoCentre A5  // HC-SR04 echo forward
 #define ENA 9 // PWM control for forward/backward
 #define ENB 10 // PWM control for right/left
-// #define MAX_V 255
-// #define MIN_V
 
-char *command;
-int direc = 0;
-int speed = 0;
-// float angle = 0;
-char i;
-long timeRight, timeLeft, timeBack;
-int distanceRight, distanceLeft, distanceBack;
+String fullMessageReceivedFromApp;
+char characterReceivedFromApp;
 
-void _stop();
-void forward();
-void back();
-void forwardRight();
-void forwardLeft();
-void backRight();
-void backLeft();
+char leftOrRight;
+char forwardOrBackward;
+
+String speedString;
+int speed = 999;
+
+long timeRight, timeLeft, timeCentre;
+int distanceRight, distanceLeft, distanceCentre;
 
 void _stop(){
 	digitalWrite(IN1, LOW);
@@ -38,217 +43,183 @@ void _stop(){
 	digitalWrite(IN3, LOW);
 	digitalWrite(IN4, LOW);
 }
-void forward(){
+void moveForward(){
 	digitalWrite(IN1, LOW);
-	digitalWrite(IN2, HIGH);
+	analogWrite(IN2, speed);
 	digitalWrite(IN3, LOW);
 	digitalWrite(IN4, LOW);
 }
-void back(){
-	digitalWrite(IN1, HIGH);
-	digitalWrite(IN2, LOW);
-	digitalWrite(IN3, LOW);
-	digitalWrite(IN4, LOW);
-}
-void forwardRight(){
+void moveForwardLeft(){
 	digitalWrite(IN1, LOW);
-	digitalWrite(IN2, HIGH);
-	digitalWrite(IN3, LOW);
-	digitalWrite(IN4, HIGH);
-}
-void forwardLeft(){
-	digitalWrite(IN1, LOW);
-	digitalWrite(IN2, HIGH);
+	analogWrite(IN2, speed);
 	digitalWrite(IN3, HIGH);
 	digitalWrite(IN4, LOW);
 }
-void backRight(){
-	digitalWrite(IN1, HIGH);
-	digitalWrite(IN2, LOW);
+void moveForwardRight(){
+	digitalWrite(IN1, LOW);
+	analogWrite(IN2, speed);
 	digitalWrite(IN3, LOW);
 	digitalWrite(IN4, HIGH);
+	delay(50);
 }
-void backLeft(){
-	digitalWrite(IN1, HIGH);
+void moveBackward(){
+	analogWrite(IN1, speed);
+	digitalWrite(IN2, LOW);
+	digitalWrite(IN3, LOW);
+	digitalWrite(IN4, LOW);
+}
+void moveBackwardLeft(){
+	analogWrite(IN1, speed);
 	digitalWrite(IN2, LOW);
 	digitalWrite(IN3, HIGH);
 	digitalWrite(IN4, LOW);
+}
+void moveBackwardRight(){
+	analogWrite(IN1, speed);
+	digitalWrite(IN2, LOW);
+	digitalWrite(IN3, LOW);
+	digitalWrite(IN4, HIGH);
 }
 
 void autonomous(int distanceRight, int distanceLeft, int distanceCentre, int ENAspeed, int ENBspeed){
 	if(distanceRight >= 5 && distanceLeft >= 5){
-		if(distanceBack >= 5) analogWrite(ENA, 150);
+		if(distanceCentre >= 5) analogWrite(ENA, 150);
 		else analogWrite(ENA, 100);
-		forward();
+		moveForward();
 	}else if(distanceRight < 5 && distanceLeft >= 5){
 		while(distanceRight < 5){
 			analogWrite(ENB, 100);
-			forwardLeft();
+			moveForwardLeft();
 			delay(5);
-			backRight();
+			moveBackRight();
 			delay(5);
 		}
 	}else if(distanceRight >= 5 && distanceLeft < 5){
 		while(distanceLeft < 5){
 			analogWrite(ENB, 100);
-			forwardRight();
+			moveForwardRight();
 			delay(5);
-			backLeft();
+			moveBackLeft();
 			delay(5);
 		}
 	}else{
-		if(distanceBack >= 5){
+		if(distanceCentre >= 5){
 			while(distanceRight <5 && distanceLeft < 5){
 				analogWrite(ENB, 100);
-				forwardRight();
+				moveForwardRight();
 				delay(5);
 				analogWrite(ENB, 150);
-				backLeft();
+				moveBackLeft();
 				delay(5);
 			}
 		}else{
-			while(distanceRight <5 && distanceLeft < 5 && distanceBack < 5){
+			while(distanceRight <5 && distanceLeft < 5 && distanceCentre < 5){
 				analogWrite(ENB, 100);
-				forwardRight();
+				moveForwardRight();
 				delay(5);
-				backLeft();
+				moveBackLeft();
 				delay(5);
 			}
 		}
 	}
 }
 
-void setup(){
-	Serial.begin(9600);
+void setup() {
+	bluetoothConnection.begin(9600); // set up Serial library at 9600 bps
 	
-	// HC-06 default serial speed is 9600
-	mySerial.begin(9600);
+	pinMode(LED_BUILTIN, OUTPUT);
 	
 	pinMode(IN1, OUTPUT);
 	pinMode(IN2, OUTPUT);
 	pinMode(IN3, OUTPUT);
 	pinMode(IN4, OUTPUT);
-	pinMode(ENA, OUTPUT);
-	pinMode(ENB, OUTPUT);
 	pinMode(trigRight, OUTPUT);
 	pinMode(echoRight, INPUT);
 	pinMode(trigLeft, OUTPUT);
 	pinMode(echoLeft, INPUT);
-	pinMode(trigBack, OUTPUT);
-	pinMode(echoBack, INPUT);
+	pinMode(trigCentre, OUTPUT);
+	pinMode(echoCentre, INPUT);
+	pinMode(ENA, OUTPUT);
+	pinMode(ENB, OUTPUT);
 }
 
 void loop(){
-	// manual Bluetooth controls via Android app if input is available, else autonomous
-	/*int index = 0;
-	while(Serial.available() > 0){
-		command[index++] = Serial.read();
-	}
-	speed = atoi(&command[2])*100+atoi(&command[3])*10+atoi(&command[4])*1;
-	if(strlen(command) == 1){
-		_stop();
-		// break;
-	}
-	else if(command[0] == '0'){
-		if(command[1] == '0'){ //00 forward
-			analogWrite(ENA, speed);
-			forward();
-			// break;
+	if(bluetoothConnection.available()){
+		fullMessageReceivedFromApp = "";
+		while bluetoothConnection.available()){
+			characterReceivedFromApp = ((byte) bluetoothConnection.read());
+			if(characterReceivedFromApp == '>') break;
+			else fullMessageReceivedFromApp += characterReceivedFromApp;
+			delay(1);
 		}
-		else if(command[1] == '2'){ //02 forward left
-			analogWrite(ENA, speed);
-			turnLeft();
-			// break;
-		}
-		else if(command[1] == '3'){ //03 forward right
-			analogWrite(ENA, speed);
-			turnRight();
-			// break;
-		}
-	}
-	else if(command[0] == '1'){
-		if(command[1] == '1'){ //11 backward
-			analogWrite(ENA, speed);
-			back();
-			// break;
-		}
-		else if(command[1] == '2'){ //12 backleft
-			analogWrite(ENA, speed);
-			backLeft();
-			// break;
-		}
-		else if(command[1] == '3'){ //13 backright
-			analogWrite(ENA, speed);
-			backRight();
-			// break;
-		}
-	}*/
-	
-	
-	/////////////// old manual controls via Bluetooth
-	/*
-	if(mySerial.available()>0){
-		i = (char)mySerial.read();
-    
-		switch(i){
-			case'1':{
-				forward();
-				break;
-			}
-			case'2':{
-				back();
-				break;
-			}
-			case'3':{
-				forwardRight();
-				break;
-			}
-			case'4':{
-				forwardLeft();
-				break;
-			}
-			case'5':{
-				backRight();
-				break;
-			}
-			case'6':{
-				backLeft();
-				break;
-			}
-			case'7':{
+		
+		leftOrRight = fullMessageReceivedFromApp[0];
+		forwardOrBackward = fullMessageReceivedFromApp[1];
+		speedString = fullMessageReceivedFromApp.substring(2);
+		speed = speedString.toInt();
+		
+		if(speed < 120) speed = 120;
+		
+		switch(forwardOrBackward){
+			case STOP:
 				_stop();
 				break;
-			}
+			case FORWARD: 
+				switch(leftOrRight){
+					case STRAIGHT: 
+						moveForward();
+						break;
+					case LEFT: 
+						moveForwardLeft();
+						break;
+					case RIGHT:
+						moveForwardRight();
+						break;
+				}
+				break;
+			case BACKWARD:
+				switch(leftOrRight){
+					case STRAIGHT:
+						moveBackward();
+						break;
+					case LEFT:
+						moveBackwardLeft();
+						break;
+					case RIGHT:
+						moveBackwardRight();
+						break;
+				}
+				break;
 		}
-	}else{*/
-		// ultrasonic range sensor
+	}else{
 		digitalWrite(trigRight, LOW);
 		digitalWrite(trigLeft, LOW);
-		digitalWrite(trigBack, LOW);
+		digitalWrite(trigCentre, LOW);
 		delayMicroseconds(2);
 		
 		digitalWrite(trigRight, HIGH);
 		digitalWrite(trigLeft, HIGH);
-		digitalWrite(trigBack, HIGH);
+		digitalWrite(trigCentre, HIGH);
 		delayMicroseconds(10);
 		digitalWrite(trigRight, LOW);
 		digitalWrite(trigLeft, LOW);
-		digitalWrite(trigBack, LOW);
+		digitalWrite(trigCentre, LOW);
 		
 		timeRight = pulseIn(echoRight, HIGH);
 		timeLeft = pulseIn(echoLeft, HIGH);
-		timeBack = pulseIn(echoBack, HIGH);
+		timeCentre = pulseIn(echoCentre, HIGH);
 		distanceRight = timeRight*0.034/2;
 		distanceLeft = timeLeft*0.034/2;
-		distanceBack = timeBack*0.034/2;
+		distanceCentre = timeCentre*0.034/2;
 		
 		Serial.print("Distance right: ");
 		Serial.println(distanceRight);
 		Serial.print("Distance left: ");
 		Serial.println(distanceLeft);
-		Serial.print("Distance back: ");
-		Serial.println(distanceBack);
+		Serial.print("Distance centre: ");
+		Serial.println(distanceCentre);
 		autonomous(distanceRight, distanceLeft, distanceCentre, 0, 0);
-	//}
+	}
 }
+
 
