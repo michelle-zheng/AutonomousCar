3ef897c00c378d2fb02f6b18891bb342ae3a7cbe
Michelle Zheng <mqzheng.edu.uwaterloo.ca>
2017-11-23 01:45:43 -0500

    Fully functional autonomous and phone control

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -5,6 +5,7 @@
 #include <NewPing.h>
 
 SoftwareSerial bluetoothConnection(12, 13); // RX, TX
+
 #define IN2 3        // L298N Forward  (green)
 #define IN3 4        // L298N Left (yellow)
 #define IN4 5        // L298N Right (orange)
@@ -30,165 +31,242 @@ SoftwareSerial bluetoothConnection(12, 13); // RX, TX
 
 String fullMessageReceivedFromApp;
 char characterReceivedFromApp;
+
 char leftOrRight;
 char forwardOrBackward;
 String speedString;
-int speed = 999;
+int speed = 0;
+bool isStopped = false;
 
 NewPing sensors[3] = {
-	NewPing(trigLeft, echoLeft),
-	NewPing(trigCentre, echoCentre),
-	NewPing(trigRight, echoRight)
+  NewPing(trigLeft, echoLeft),
+  NewPing(trigCentre, echoCentre),
+  NewPing(trigRight, echoRight)
 };
 
-unsigned long distances[3];
-bool tooClose;
+unsigned long distancesFromObstacle[3];
+bool tooCloseToObstacle;
 
 void _stop(){
-	digitalWrite(IN1, LOW);
-	digitalWrite(IN2, LOW);
-	digitalWrite(IN3, LOW);
-	digitalWrite(IN4, LOW);
-}
-void moveForward(){
-	digitalWrite(IN1, LOW);
-	analogWrite(IN2, speed);
-	digitalWrite(IN3, LOW);
-	digitalWrite(IN4, LOW);
-}
-void moveForwardLeft(){
-	digitalWrite(IN1, LOW);
-	analogWrite(IN2, speed);
-	digitalWrite(IN3, HIGH);
-	digitalWrite(IN4, LOW);
-}
-void moveForwardRight(){
-	digitalWrite(IN1, LOW);
-	analogWrite(IN2, speed);
-	digitalWrite(IN3, LOW);
-	digitalWrite(IN4, HIGH);
-	delay(50);
-}
-void moveBackward(){
-	analogWrite(IN1, speed);
-	digitalWrite(IN2, LOW);
-	digitalWrite(IN3, LOW);
-	digitalWrite(IN4, LOW);
-}
-void moveBackwardLeft(){
-	analogWrite(IN1, speed);
-	digitalWrite(IN2, LOW);
-	digitalWrite(IN3, HIGH);
-	digitalWrite(IN4, LOW);
-}
-void moveBackwardRight(){
-	analogWrite(IN1, speed);
-	digitalWrite(IN2, LOW);
-	digitalWrite(IN3, LOW);
-	digitalWrite(IN4, HIGH);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
 
-void autonomous(int distances[]){
-	speed = 150;
-	if(distances[CENTRE_SENSOR] >= 10 && distances[CENTRE_SENSOR] <= 15){                   // if distance centre small move forward slowly
-		speed = 120;
-		moveForward();
-	}else if(distances[CENTRE_SENSOR] > 15) moveFoward();                         // if distance centre greater move forward normally
-	else if(distances[CENTRE_SENSOR]<=15 && distances[RIGHT_SENSOR]<=15 && distances[LEFT_SENSOR]>15) moveFowardLeft(); // if only distance left big turn left
-	else if(distances[CENTRE_SENSOR]<=15 && distances[LEFT_SENSOR]<=15 && distances[RIGHT_SENSOR]>15) moveFowardRight(); // if only distance right big turn right
-	else{                                                               // else turn right in place until distance centre is larger
-		while(distances[CENTRE_SENSOR] < 10){
-			speed = 120;
-			moveForwardRight();
-			delay(20);
-			moveBackwardLeft();
-			delay(20);
-		}
-	}
+void moveForward(){
+  digitalWrite(IN1, LOW);
+  analogWrite(IN2, speed);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void moveForwardLeft(){
+  digitalWrite(IN1, LOW);
+  analogWrite(IN2, speed);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void moveForwardRight(){
+  digitalWrite(IN1, LOW);
+  analogWrite(IN2, speed);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void moveBackward(){
+  analogWrite(IN1, speed);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void moveBackwardLeft(){
+  analogWrite(IN1, speed);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void moveBackwardRight(){
+  analogWrite(IN1, speed);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+int smallestDistanceFromObstacle(unsigned long distancesFromObstacle[]){
+  int smallestDistanceIndex = 0;
+  for (int currentSensor = 1; currentSensor < 3; currentSensor++){
+    if (distancesFromObstacle[currentSensor] < distancesFromObstacle[smallestDistanceIndex]){
+      smallestDistanceIndex = currentSensor;
+    }
+  }
+  return smallestDistanceIndex;
+}
+
+void avoidObstacle(unsigned long distancesFromObstacle[]){
+  speed = 255;
+  int smallestDistanceIndex = smallestDistanceFromObstacle(distancesFromObstacle);
+  switch (smallestDistanceIndex) {
+    case LEFT_SENSOR:
+      moveBackwardLeft();
+      delay(400);
+      _stop();
+      break;
+    case CENTRE_SENSOR:
+      moveBackward();
+      delay(400);
+      _stop();
+      break;
+    case RIGHT_SENSOR:
+      moveBackwardRight();
+      delay(400);
+      _stop();
+      break;      
+  }   
+}
+
+void autonomousMovement(unsigned long distancesFromObstacle[]){
+  speed = 255;
+  int smallestDistanceIndex = smallestDistanceFromObstacle(distancesFromObstacle);
+  switch (smallestDistanceIndex) {
+    case LEFT_SENSOR:
+      moveBackwardLeft();
+      delay(700);
+      speed = 210;
+      moveForwardRight();
+      delay(700);
+      break;
+    case CENTRE_SENSOR:
+      moveBackwardLeft();
+      delay(700);
+      speed = 210;
+      for(int currentSensor = 0; currentSensor < 3; currentSensor++){
+        distancesFromObstacle[currentSensor] = sensors[currentSensor].ping_cm();
+      }
+      if (distancesFromObstacle[LEFT_SENSOR] > distancesFromObstacle[RIGHT_SENSOR]) moveForwardLeft();          
+      else moveForwardRight();
+      delay(700);
+      break;
+    case RIGHT_SENSOR:
+      moveBackwardRight();
+      delay(700);
+      speed = 210;
+      moveForwardLeft();
+      delay(700);
+      break;      
+  }   
 }
 
 void setup(){
-	Serial.begin(9600);
-	bluetoothConnection.begin(9600); // set up Serial library at 9600 bps
-	
-	pinMode(IN1, OUTPUT);
-	pinMode(IN2, OUTPUT);
-	pinMode(IN3, OUTPUT);
-	pinMode(IN4, OUTPUT);
-	
-	pinMode(trigRight, OUTPUT);
-	pinMode(echoRight, INPUT);
-	pinMode(trigLeft, OUTPUT);
-	pinMode(echoLeft, INPUT);
-	pinMode(trigCentre, OUTPUT);
-	pinMode(echoCentre, INPUT);
+  bluetoothConnection.begin(9600); // set up Serial library at 9600 bps
+  Serial.begin(9600);
+  
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  pinMode(trigRight, OUTPUT);
+  pinMode(echoRight, INPUT);
+  pinMode(trigLeft, OUTPUT);
+  pinMode(echoLeft, INPUT);
+  pinMode(trigCentre, OUTPUT);
+  pinMode(echoCentre, INPUT);
 }
 
 void loop(){
-	tooClose = false;
 
-	for(int currentSensor = 0; currentSensor < 3; currentSensor++){
-		distances[currentSensor] = sensors[currentSensor].ping_cm();
-		if(currentSensor == CENTRE_SENSOR) Serial.print("Distance Centre: ");
-		else if(currentSensor == LEFT_SENSOR) Serial.print("Distance Left: ");
-		else Serial.print("Distance Right: ");
-		Serial.println(distances[currentSensor]);
-	}
-	
-	if(distances[CENTRE_SENSOR]<15 && bluetoothConnection.available()){
-		tooClose = true;
-		speed = 200;
-		moveBackward();
-		delay(400);
-		_stop();
-		break;
-	}else if(bluetoothConnection.available()){
-		fullMessageReceivedFromApp = "";
-		while(bluetoothConnection.available()){
-			characterReceivedFromApp = ((byte) bluetoothConnection.read());
-			if(characterReceivedFromApp == '>') break;
-			else fullMessageReceivedFromApp += characterReceivedFromApp;
-			delay(1);
-		}
-		
-		leftOrRight = fullMessageReceivedFromApp[0];
-		forwardOrBackward = fullMessageReceivedFromApp[1];
-		speedString = fullMessageReceivedFromApp.substring(2);
-		speed = speedString.toInt();
-		
-		if(speed < 120) speed = 120;
-		
-		switch(forwardOrBackward){
-			case STOP:
-				_stop();
-				break;
-			case FORWARD:
-				switch(leftOrRight){
-					case STRAIGHT:
-						moveForward();
-						break;
-					case LEFT:
-						moveForwardLeft();
-						break;
-					case RIGHT:
-						moveForwardRight();
-						break;
-				}
-				break;
-			case BACKWARD:
-				switch(leftOrRight){
-					case STRAIGHT:
-						moveBackward();
-						break;
-					case LEFT:
-						moveBackwardLeft();
-						break;
-					case RIGHT:
-						moveBackwardRight();
-						break;
-				}
-				break;
-		}
-	}else if(!bluetoothConnection.available()) //autonomous(distances);
+  tooCloseToObstacle = false;
+
+  // Get distance from obstacle
+  for(int currentSensor = 0; currentSensor < 3; currentSensor++){
+    distancesFromObstacle[currentSensor] = sensors[currentSensor].ping_cm();
+  }
+
+  for (int currentSensor = 0; currentSensor < 3; currentSensor++){
+    if(distancesFromObstacle[currentSensor] < 40){
+      speed = 120;
+    }
+    if(distancesFromObstacle[currentSensor] < 25 && distancesFromObstacle[currentSensor] != 0){
+      tooCloseToObstacle = true;
+      break;
+    }
+  }
+  Serial.println(tooCloseToObstacle);
+  
+  if (bluetoothConnection.available()){
+    if (tooCloseToObstacle == false){
+      fullMessageReceivedFromApp = "";
+      while (bluetoothConnection.available()){
+        characterReceivedFromApp = ((byte) bluetoothConnection.read());
+        if (characterReceivedFromApp == '>'){
+          break;
+        }
+        else {
+          fullMessageReceivedFromApp += characterReceivedFromApp;
+        }
+        delay(1);
+      }
+  
+      leftOrRight = fullMessageReceivedFromApp[0];
+      forwardOrBackward = fullMessageReceivedFromApp[1];
+      speedString = fullMessageReceivedFromApp.substring(2);
+      speed = speedString.toInt();
+      
+      if (speed < 120){
+        speed = 120;
+      }
+  
+      switch (forwardOrBackward) {
+        case '9':
+          _stop();
+          isStopped = true;
+          break;
+        case FORWARD: 
+          switch (leftOrRight) {
+            case STRAIGHT: 
+              moveForward();
+              break;
+            case LEFT: 
+              moveForwardLeft();
+              break;
+            case RIGHT:
+              moveForwardRight();
+              break;
+          }
+          break;
+        case BACKWARD: 
+          switch (leftOrRight) {
+            case STRAIGHT:
+            moveBackward();
+            break;
+          case LEFT:
+            moveBackwardLeft();
+            break;
+          case RIGHT:
+            moveBackwardRight();
+            break;  
+          }
+        break;
+      }
+    }
+    else {
+      avoidObstacle(distancesFromObstacle);
+    }
+  }
+  else if (isStopped == false) {
+    if (tooCloseToObstacle == true){
+      autonomousMovement(distancesFromObstacle);  
+    }
+    else {
+      speed = 130;
+      moveForward();
+    }
+  }
 }
 
